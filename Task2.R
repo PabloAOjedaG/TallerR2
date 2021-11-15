4f774e4272d4762254c141a7bcfe8221625188ae
GabrielPerdomo <88752555+GabrielPerdomo@users.noreply.github.com>
2021-11-15 12:23:05 -0500

    Update Task2.R

--- Task2.R
+++ Task2.R
@@ -15,15 +15,25 @@ Sys.setlocale("LC_CTYPE", "en_US.UTF-8")
 #-------- PUNTO 1 ---------
 #--------------------------
 
+# Configuraciones iniciales
+rm(list = ls())
+if(!require(pacman)) install.packages("pacman") ; require(pacman)
+p_load(rio,tidyverse,skimr, qpcR, read_xls)
+Sys.setlocale("LC_CTYPE", "en_US.UTF-8")
+
+#--------------------------
+#-------- PUNTO 1 ---------
+#--------------------------
+
 chip <- list ()
 
-
 #listar todos los archivos xls en el directorio actual
-list.archivos(pattern="xls") #argumento para definir i
+ #argumento para definir i
+listxls<-dir(pattern="xls")
+ # se crea una lista con los archivos
 
-list.archivos<-list.archivos(pattern="xls") # se crea una lista con los archivos
-
-for (i in 1:length(list.archivos))
-{chip[[i]]<-read_xls(list.archivos[i])}
 # se crea un loop para leer los archivos de excel.
-
+for (k in 1:length(listxls)){
+  chip[[k]] <- read.csv(listxls[k])
+}
+str(chip[[1]]) 
