e2d3a2f6c8c9410c6eabc8e6c81da368351a4417
dlsanchezr1 <dl.sanchezr1@uniandes.edu.co>
2021-11-15 16:14:35 -0500

    Punto 2

--- Task2.R
+++ Task2.R
@@ -16,19 +16,49 @@ Sys.setlocale("LC_CTYPE", "en_US.UTF-8")
 #-------- PUNTO 1 ---------
 #--------------------------
 
+#Creamos la lista vacía
 chip <- list ()
 
-#listar todos los archivos xls en el directorio actual
-# argumento para definir k
+#Listamos todos los archivos xls en el directorio actual
+#Argumento para definir 
 listxls <- list.files(pattern="*.xls", recursive = TRUE)
 
-# comando inspirado por la página StackOverflow. Link:
+#Comando inspirado por la página StackOverflow. Link:
 browseURL("https://stackoverflow.com/questions/32888757/how-can-i-read-multiple-excel-files-into-r")
 
-# se crea una lista con los archivos
-
-# se crea un loop para leer los archivos de excel.
+#Creamos un loop para leer los archivos de excel.
 for (k in 1:length(listxls)){
   chip[[k]] = import(file = listxls[k])
 }
-str(chip[[1]]) 
+
+#--------------------------
+#-------- PUNTO 2 ---------
+#--------------------------
+
+#Creamos la función que nos saque el valor de Pago para Educación 
+f_extrac = function(i,lista,categoria){
+  lista_i = lista[[i]] 
+  colnames(lista_i) = lista_i[7,]
+  pago = lista_i %>% subset(NOMBRE==categoria) %>% select(`PAGOS(Pesos)`)
+  return(pago)
+}
+f_extrac(i = 10 , lista = chip , categoria = "EDUCACIÓN")
+
+#Ahora, le agregamos a la función que nos saque el código DANE y el periodo
+f_extrac = function(i,lista,categoria){
+  
+#Creamos el dataframe
+df = data.frame(pago=NA,codigo_dane=NA,periodo=NA)  
+lista_i = lista[[i]] 
+  
+#Extraemos el código, el periodo y el valor del pago
+df$codigo_dane = colnames(lista_i)[1]
+  
+df$periodo = lista_i[2,1]
+  
+colnames(lista_i) = lista_i[7,]
+df$pago = lista_i %>% subset(NOMBRE==categoria) %>% select(`PAGOS(Pesos)`)
+  
+  return(df)  
+}
+f_extrac(i = 10 , lista = chip , categoria = "EDUCACIÓN")
